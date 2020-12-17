ae66501f54fa34815d9cc1544dd0e0a6a1c83810
Nathan Schrader <nathanschrader@icloud.com>
2020-12-17 00:12:43 -0800

    add logic to CalculatePastryOrder()

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -8,10 +8,26 @@ namespace Bakery
   {
     public string Flavor { get; set; }
     public double Price { get; set; }
-
-    public Pastry()
+    private static List<Pastry> _instances = new List<Pastry> {};
+    private static List<Pastry> _pastryOrder = new List<Pastry> {};
+    public Pastry(string flavor, double price)
     {
-      
+      Flavor = flavor;
+      Price = price;
+    }
+    public double CalculatePastryOrder(int quantity)
+    {
+      int numberOfFullSets = quantity / 3;
+      int remainingPastries = quantity % 3;
+      return (numberOfFullSets * 5) + (remainingPastries * Price);
+    }
+    public static List<Pastry> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
