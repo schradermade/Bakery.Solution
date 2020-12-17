6e9cd1bdcb2195c35cedee23b699ea0316316d20
Nathan Schrader <nathanschrader@icloud.com>
2020-12-16 21:56:15 -0800

    refactor UI code

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,34 +1,49 @@
 using System;
 using System.Collections.Generic;
 
-
 namespace Bakery
 {
   public class Programs
   {
     static void Main()
     {
-      Console.WriteLine("Welcome to Schrader's Bakery! Bread is $" + Bread.Price +  " per loaf, and Pastries are $2.50 each.");
+      Bread plainBread = new Bread("Plain", 5);
+      Pastry donut = new Pastry("Glazed Donut", 2);
+      double totalOrderCost = 0;
+      Console.WriteLine("Welcome to Schrader's Bakery! Plain bread is $" + plainBread.Price +  " per loaf, and Pastries are " + donut.Price + " each.");
       Console.WriteLine("press ENTER to order!");
       Console.ReadLine();
-      Console.WriteLine("What can we get you?");
-      Console.WriteLine("Please Type 'Bread' or 'Pastry' ");
-      string placeOrder = Console.ReadLine().ToLower();
-      if (placeOrder == "bread")
+      Console.WriteLine("Would you like to order bread today? Type 'yes' to order");
+      string breadOrder = Console.ReadLine();
+      if (breadOrder == "yes")
       {
         Console.WriteLine("Which flavor bread would you like?");
         Console.WriteLine("press ENTER to see our selection!");
         Console.ReadLine();
-        Console.WriteLine("Availabe Flavors: Sourdough");
+        Console.WriteLine("Availabe Flavors: Plain");
         Console.WriteLine("Please type in one of of the flavor selections listed.");
         string flavor = Console.ReadLine();
         Console.WriteLine("How many loafs of " + flavor + " would you like?");
-        string quantity = Console.ReadLine();
-        int quantityString = Int32.Parse(quantity);
-        int quantityDeals = Int32.Parse(quantity) / 2;
-        Bread _breadOrder = new Bread(flavor, quantity);
-        Console.WriteLine(_breadOrder.GetOrder());
+        string quantityString = Console.ReadLine();
+        int quantity = Int32.Parse(quantityString);
+        totalOrderCost += plainBread.CalculateOrderCost(quantity);
       }
+      Console.WriteLine("Would you like to order pastries today? Type 'yes' to order");
+      string pastryOrder = Console.ReadLine();
+      if (pastryOrder == "yes")
+      {
+        Console.WriteLine("Which flavor pastry would you like?");
+        Console.WriteLine("press ENTER to see our selection!");
+        Console.ReadLine();
+        Console.WriteLine("Available Flavors: Glazed Donut");
+        Console.WriteLine("Please type in one of the flavor selections listed.");
+        string flavor = Console.ReadLine();
+        Console.WriteLine("How many " + flavor + "'s would you like?");
+        string quantityString = Console.ReadLine();
+        int quantity = Int32.Parse(quantityString);
+        totalOrderCost += donut.CalculateOrderCost(quantity);
+      }
+      Console.WriteLine(totalOrderCost);
     }
   }
 }
