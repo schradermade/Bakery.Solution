49600957701450dbb9e26401c645eed2c15cc42a
Nathan Schrader <nathanschrader@icloud.com>
2020-12-17 01:20:17 -0800

    complete user interface for ordering

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,10 +7,20 @@ namespace Bakery
   {
     static void Main()
     {
+      string flavorBread = "";
+      string flavorPastry = "";
+      string quantityBreadString = "";
+      int quantityBread = 0;
+      string quantityPastryString = "";
+      int quantityPastry = 0;
       Bread plainBread = new Bread("Plain", 5);
       Pastry donut = new Pastry("Glazed Donut", 2);
-      double totalOrderCost = 0;
-      Console.WriteLine("Welcome to Schrader's Bakery! Plain bread is $" + plainBread.Price +  " per loaf, and Pastries are " + donut.Price + " each.");
+      double totalOrderCostBread = 0;
+      double totalOrderCostPastry = 0;
+      Console.WriteLine("Welcome to Schrader's Bakery! Plain bread is $" + plainBread.Price +  " per loaf, and Pastries are $" + donut.Price + " each.");
+      Console.WriteLine("Today's Deals:");
+      Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $" + plainBread.Price);
+      Console.WriteLine("Pastries: Buy 1 for $" + donut.Price + ", or 3 for $5.");
       Console.WriteLine("press ENTER to order!");
       Console.ReadLine();
       Console.WriteLine("Would you like to order bread today? Type 'yes' to order");
@@ -22,11 +32,11 @@ namespace Bakery
         Console.ReadLine();
         Console.WriteLine("Availabe Flavors: Plain");
         Console.WriteLine("Please type in one of of the flavor selections listed.");
-        string flavor = Console.ReadLine();
-        Console.WriteLine("How many loafs of " + flavor + " would you like?");
-        string quantityString = Console.ReadLine();
-        int quantity = Int32.Parse(quantityString);
-        totalOrderCost += plainBread.CalculateOrderCost(quantity);
+        flavorBread = Console.ReadLine();
+        Console.WriteLine("How many loafs of " + flavorBread + " bread would you like?");
+        quantityBreadString = Console.ReadLine();
+        quantityBread = Int32.Parse(quantityBreadString);
+        totalOrderCostBread += plainBread.CalculateOrderCost(quantityBread);
       }
       Console.WriteLine("Would you like to order pastries today? Type 'yes' to order");
       string pastryOrder = Console.ReadLine();
@@ -37,13 +47,28 @@ namespace Bakery
         Console.ReadLine();
         Console.WriteLine("Available Flavors: Glazed Donut");
         Console.WriteLine("Please type in one of the flavor selections listed.");
-        string flavor = Console.ReadLine();
-        Console.WriteLine("How many " + flavor + "'s would you like?");
-        string quantityString = Console.ReadLine();
-        int quantity = Int32.Parse(quantityString);
-        totalOrderCost += donut.CalculateOrderCost(quantity);
+        flavorPastry = Console.ReadLine();
+        Console.WriteLine("How many " + flavorPastry + "'s would you like?");
+        quantityPastryString = Console.ReadLine();
+        quantityPastry = Int32.Parse(quantityPastryString);
+        totalOrderCostPastry += donut.CalculatePastryOrder(quantityPastry);
       }
-      Console.WriteLine(totalOrderCost);
+      Console.WriteLine("********************************");
+      Console.WriteLine("**Schrader's Bakery Reciept:**");
+      Console.WriteLine("********************************");
+      Console.WriteLine("-");
+      Console.WriteLine("Loafs of " + flavorBread + " bread: " + quantityBread + " @ $" + totalOrderCostBread + " total");
+      Console.WriteLine("-");
+      Console.WriteLine("Savings on daily bread deal!: $" + ((quantityBread * plainBread.Price) - totalOrderCostBread));
+      Console.WriteLine("-");
+      Console.WriteLine("Number of " + flavorPastry + " pastries: " + quantityPastry + " @ $" + totalOrderCostPastry + " total");
+      Console.WriteLine("-");
+      Console.WriteLine("Savings from daily pastry deal!: $" + ((quantityPastry * donut.Price) - totalOrderCostPastry));
+      Console.WriteLine("-");
+      Console.WriteLine("Today's total savings!: $" + ((((quantityPastry * donut.Price) - totalOrderCostPastry)) + ((quantityBread * plainBread.Price) - totalOrderCostBread)) + ".00");
+      Console.WriteLine("-");
+      Console.WriteLine("Order Total: $" + (totalOrderCostBread + totalOrderCostPastry) + ".00");
+      Console.WriteLine("--------------------------------");
     }
   }
 }
