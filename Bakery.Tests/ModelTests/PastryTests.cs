a9446f5077ace11c199017190d73f8fae134d31f
Nathan Schrader <nathanschrader@icloud.com>
2020-12-16 23:05:40 -0800

    good fail: CalculatePastryOrder()

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,9 +1,38 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using Bakery.Models;
+using Bakery;
 using System;
 
 namespace Bakery.Tests
 {
-  
+  [TestClass]
+  public class PastryTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Pastry.ClearAll();
+    }
+
+    [TestMethod]
+    public void GetPastry_CreatesInstanceOfItem_Pastry()
+    {
+      Pastry newPastry = new Pastry("donut", 3);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+    
+    [TestMethod]
+    public void CalcOrderCost_ReturnsTotalCost_Cost()
+    {
+      // Arrange
+      string flavor = "Glazed";
+      int price = 3;
+      int OrderCost = 15;
+      Pastry newPastry = new Pastry(flavor, price);
+      // Act
+      double result = newPastry.CalculatePastryOrder(4);
+      // Assert
+      Assert.AreEqual(OrderCost, result);
+    }
+
+  }
 }
